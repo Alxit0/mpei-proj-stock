32bd11e01e35f735ff939600875bbe89e23acfec
Alexandre Regalado <alexandreregalado@hotmail.com>
2024-12-16 00:51:21 +0000

    bug in add stocks

--- bloom/bloomAddElemento.m
+++ bloom/bloomAddElemento.m
@@ -1,7 +1,7 @@
 function filtro =  bloomAddElemento(filtro,chave,k)
     for i=1:k
         chave= [chave num2str(i)];
-        code = mod(string2hash(chave),length(filtro))+1;
+        code = mod(string2hash_aux(chave, k),length(filtro))+1;
         filtro(code)=1;
         
     end
